dd112494154483e0fa0e14c9bdb04346885f0ee9
Ademacir Filho <ademacirfilho@gmail.com>
2025-01-07 17:52:29 -0300

    Adição de templates para a edição e exclusão de contatos, categorias e contas bancárias, com a implementação dos respectivos CRUDs em cada um dos templates.

--- sistemaFFA/views.py
+++ sistemaFFA/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, get_object_or_404, redirect
 from django.forms.models import model_to_dict
-from .models import Transacao, Contato, Categoria, ContaBancaria, Pagamento, TipoCategoria, Conta
+from .models import Transacao, Contato, Categoria, ContaBancaria
 from .forms import TransacaoForm, ContatoForm, CategoriaForm, ContaBancariaForm
 from django.contrib.auth.decorators import login_required
 
@@ -79,6 +79,34 @@ def contatos(request):
     
     return render(request, "contatos.html", context)
 
+def editar_contato(request, contato_id):
+    contato = get_object_or_404(Contato, pk=contato_id)
+    context = {
+        "contatos": Contato.objects.all(),
+        "form": ContatoForm(instance=contato)
+    }
+
+    if request.method == "POST":
+        form = ContatoForm(request.POST, instance=contato)
+        if form.is_valid():
+            form.save()
+            return redirect('contatos')
+        else:
+            context["form"] = form
+
+    return render(request, "editar_contato.html", context)
+
+def deletar_contato(request, contato_id):
+    context = {
+        "contatos": get_object_or_404(Contato, pk=contato_id)
+    }
+
+    if request.method == 'POST':
+        context["contatos"].delete()
+        return redirect('contatos')
+    else:
+        return render(request, "deletar_contato.html", context)
+
 def categorias(request):
     context = {
         "categorias": Categoria.objects.all()
@@ -96,6 +124,33 @@ def categorias(request):
 
     return render(request, "categorias.html", context)
 
+def editar_categoria(request, categoria_id):
+    categoria = get_object_or_404(Categoria, pk=categoria_id)
+    context = {
+        "categorias": Categoria.objects.all(),
+        "form": CategoriaForm(instance=categoria)
+    }
+
+    if request.method == "POST":
+        form = CategoriaForm(request.POST, instance=categoria)
+        if form.is_valid():
+            form.save()
+            return redirect('categorias')
+        else:
+            context["form"] = form
+
+    return render(request, "editar_categoria.html", context)
+
+def deletar_categoria(request, categoria_id):
+    context = {
+        "categorias": get_object_or_404(Categoria, pk=categoria_id)
+    }
+
+    if request.method == 'POST':
+        context["categorias"].delete()
+        return redirect('categorias')
+    else:
+        return render(request, "deletar_categoria.html", context)
 
 def conta_bancaria(request):
     context = {
@@ -112,4 +167,32 @@ def conta_bancaria(request):
     else:
         context["form"] = ContaBancariaForm
 
-    return render(request, "conta-bancaria.html", context)
+    return render(request, "conta_bancaria.html", context)
+
+def editar_conta_bancaria(request, conta_bancaria_id):
+    conta_bancaria = get_object_or_404(ContaBancaria, pk=conta_bancaria_id)
+    context = {
+        "contas_bancarias": ContaBancaria.objects.all(),
+        "form": ContaBancariaForm(instance=conta_bancaria)
+    }
+
+    if request.method == "POST":
+        form = ContaBancariaForm(request.POST, instance=conta_bancaria)
+        if form.is_valid():
+            form.save()
+            return redirect('conta_bancaria')
+        else:
+            context["form"] = form
+
+    return render(request, "editar_conta_bancaria.html", context)
+
+def deletar_conta_bancaria(request, conta_bancaria_id):
+    context = {
+        "contas_bancarias": get_object_or_404(ContaBancaria, pk=conta_bancaria_id)
+    }
+
+    if request.method == 'POST':
+        context["contas_bancarias"].delete()
+        return redirect('conta_bancaria')
+    else:
+        return render(request, "deletar_conta_bancaria.html", context)
